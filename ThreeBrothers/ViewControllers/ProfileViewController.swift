5c54ef994325df3c9d51c48a4ec1b7011160686a
komplektan <komplektan@mail.ru>
2022-05-27 22:38:44 +0500

    Other board

--- ThreeBrothers/ViewControllers/ProfileViewController.swift
+++ ThreeBrothers/ViewControllers/ProfileViewController.swift
@@ -15,6 +15,8 @@ final class ProfileViewController: UIViewController {
     private let nameTextField: UITextField = {
         let textField = UITextField.customTextField()
         textField.placeholder = "Ваше имя"
+        textField.layer.borderWidth = 2
+        textField.layer.masksToBounds = true
         textField.clearButtonMode = .whileEditing
         return textField
     }()
@@ -87,24 +89,12 @@ final class ProfileViewController: UIViewController {
     }
 }
 
-class customUITextField: UITextField {
-
-        func setup() {
-            let border = CALayer()
-            let width = CGFloat(2.0)
-        border.borderColor = UIColor.darkGray.cgColor
-        border.frame = CGRect(x: 0, y: self.frame.size.height - width, width: self.frame.size.width, height: self.frame.size.height)
-        border.borderWidth = width
-        self.layer.addSublayer(border)
-        self.layer.masksToBounds = true
-    }
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setup()
-    }
-    required public init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-        setup()
+extension UITextField {
+    func addBottomBorder() {
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect(x: 0, y: self.frame.size.height - 1, width: self.frame.size.width, height: 1)
+        bottomLine.backgroundColor = UIColor.black.cgColor
+        borderStyle = .none
+        layer.addSublayer(bottomLine)
     }
 }
