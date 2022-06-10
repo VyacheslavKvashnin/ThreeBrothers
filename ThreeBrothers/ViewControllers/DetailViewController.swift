40b56857aa271156be5cab51eef76bbad2fd8a4d
komplektan <komplektan@mail.ru>
2022-06-10 23:41:49 +0500

    Dependency injection

--- ThreeBrothers/ViewControllers/DetailViewController.swift
+++ ThreeBrothers/ViewControllers/DetailViewController.swift
@@ -8,7 +8,18 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
+    
+    var product: Product
+    
+    init(product: Product) {
+        self.product = product
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .purple
--- ThreeBrothers/ViewControllers/MainViewController.swift
+++ ThreeBrothers/ViewControllers/MainViewController.swift
@@ -55,7 +55,8 @@ extension MainViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        let detailVC = DetailViewController()
+
+        let detailVC = DetailViewController(product: <#Product#>)
         navigationController?.pushViewController(detailVC, animated: true)
     }
 }
