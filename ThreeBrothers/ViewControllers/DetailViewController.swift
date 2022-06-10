ce9485c7ef39b01206630301429e6064853d769c
komplektan <komplektan@mail.ru>
2022-06-10 22:50:00 +0500

    Add DetailVC

---
+++ ThreeBrothers/ViewControllers/DetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.swift
+//  ThreeBrothers
+//
+//  Created by Вячеслав Квашнин on 10.06.2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
