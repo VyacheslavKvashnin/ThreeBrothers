61c8fea85ee18fbdbb171ff9c1ce10b9c51c2f6d
komplektan <komplektan@mail.ru>
2022-06-17 00:38:43 +0500

    Good night

--- ThreeBrothers/ViewControllers/PhoneViewController.swift
+++ ThreeBrothers/ViewControllers/PhoneViewController.swift
@@ -132,7 +132,7 @@ final class PhoneViewController: UIViewController {
                 return
             }
             DispatchQueue.main.async {
-                let smsCodeVC = SMSCodeViewController(authManager: self!.authManager)
+                let smsCodeVC = SMSCodeViewController()
                 self?.navigationItem.backBarButtonItem = UIBarButtonItem(
                     title: "Назад",
                     style: .plain,
